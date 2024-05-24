38cc3e71a3bc01d11fdab52eca097e8611cba113
unknown <sedighe.ghanbary@gmail.com>
2024-05-24 20:23:47 +0400

    Add cicd

--- src/_old/startup/db.js
+++ src/_old/startup/db.js
@@ -7,7 +7,7 @@ exports.db = function (callback) {
   //sgh
   const db = config.get("db");
   const db_server = config.get("db_SERVER");
-   mongoose.set('strictQuery', false);
+  mongoose.set('strictQuery', false);
   mongoose
     .connect(db_server, {
       useNewUrlParser: true,
