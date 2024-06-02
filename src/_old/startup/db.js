da225749dc20c709952833cb38837e5b8c5343be
unknown <sedighe.ghanbary@gmail.com>
2024-06-02 11:58:16 +0400

    1403-03-13 2 Ghanbari

--- src/_old/startup/db.js
+++ src/_old/startup/db.js
@@ -2,47 +2,41 @@ const mongoose = require("mongoose");
 const config = require("config");
 const logger = require("../middleware/logger");
 
+exports.db = async function (callback) {
+  let db = "";
+  const environment_name = config.get("environment_name");
 
-exports.db = function async(callback) {
-
-  //sgh
-  let db = ""
-  let environment_name = config.get("environment_name")
   if (environment_name === "local") {
     db = config.get("db");
-  }
-  if (environment_name === "server") {
+  } else if (environment_name === "server") {
     db = config.get("db_SERVER");
     console.log(3, db);
   }
 
-
   try {
     mongoose.set('strictQuery', false);
-    await mongoose
-      .connect(db, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        serverSelectionTimeoutMS: 30000,
-        authSource: "admin"
-      })
-      .then(() => {
-        logger.info(`connected to database`);
-        if (callback != null) {
-          callback();
-        }
-      });
+
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000,
+      authSource: "admin"
+    });
+
+    logger.info(`Connected to database`);
     console.log('Successfully connected to MongoDB');
+
+    if (callback) {
+      callback();
+    }
   } catch (error) {
     if (error instanceof mongoose.Error.MongooseServerSelectionError) {
+      logger.error('MongooseServerSelectionError:', error.message);
       console.error(100, 'MongooseServerSelectionError:', error.message);
     } else {
+      logger.error('Unexpected Error:', error);
       console.error(200, 'Unexpected Error:', error);
     }
     process.exit(1); // Exit process with failure code
   }
-
-
-
-
 };
